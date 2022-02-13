03d12410f2a22d9f315e0e3783e7ede7e263ab33
dgh <499211616@qq.com>
2022-02-13 16:16:15 +0800

    丁国洪 提交代码

---
+++ src/main/java/com/macro/mall/tiny/MallDghApplication.java
@@ -0,0 +1,14 @@
+package com.macro.mall.tiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class MallDghApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallDghApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dgh/mall_dgh/MallDghApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgh.mall_dgh;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallDghApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
